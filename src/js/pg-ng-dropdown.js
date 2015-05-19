eeb9a5e7558058096166d092d05e175fec9f25aa
Rafael Violato <rfviolato@gmail.com>
2015-05-19 10:14:37 -0300

    Add directive template and dynamic binding for options and its texts

--- src/js/pg-ng-dropdown.js
+++ src/js/pg-ng-dropdown.js
@@ -8,16 +8,31 @@
 
 	function dropdownDirective(){
 
+		var template = '<div class="pg-dropdown">' +
+							'<div class="current-selected-option">' +
+							'</div>' +
+							'<ul class="dropdown-content">' +
+								'<li data-ng-repeat="option in data">' +
+									'<span data-ng-bind="option.text">' +
+									'</span>' +
+								'</li>' +
+							'</ul>' +
+					   '</div>';
+
 		var directive = {
+			scope: {
+				data: '=options',
+			},
 			restrict: 'AEC',
 			controller: controller,
 			controllerAs: 'dropdownCtrl',
 			link: postLink,
+			template: template,
 		};
 
 		return directive;
 
-		function controller(){
+		function controller($scope){
 			
 		}
 
