6f078631b1b77b512311b6c737c0e8d6d4f6a929
Rafael Violato <rfviolato@gmail.com>
2015-06-10 11:38:15 -0300

    Add custom arrow

--- src/js/pg-ng-dropdown.js
+++ src/js/pg-ng-dropdown.js
@@ -15,20 +15,20 @@
 		var template = [
 
 			'<div class="pg-dropdown">',
-				'<div data-ng-click="ctrl.toggle()" class="current-selected-option">',
-						'<i data-ng-if="ctrl.image == \'true\'" data-ng-style="{\'background-image\': \'url(\'+(ctrl.data[ctrl.value][ctrl.imageProperty])+\')\'}">',
+				'<div data-ng-click="toggle()" class="current-selected-option">',
+						'<i data-ng-if="image == \'true\'" data-ng-style="{\'background-image\': \'url(\'+(data[value][imageProperty])+\')\'}">',
 						'</i>',
-						'<span data-ng-bind="ctrl.data[ctrl.value][ctrl.textProperty] || ctrl.value">',
+						'<span data-ng-bind="data[value][textProperty] || value">',
 						'</span>',
 						'<div class="arrow-wrapper">',
-							'<div class="arrow"></div>',
+							'<div class="arrow ss-icon ss-standard ss-navigatedown"></div>',
 						'</div>',
 				'</div>',
 				'<ul class="dropdown-content">',
-					'<li data-ng-click="ctrl.selectOption($index)" data-ng-repeat="option in ctrl.data" title="{{option[ctrl.textProperty]}}" >',
-						'<i data-ng-if="ctrl.image == \'true\'" data-ng-style="{\'background-image\': \'url(\'+(option[ctrl.imageProperty])+\')\'}">',
+					'<li data-ng-click="selectOption($index)" data-ng-repeat="option in data" title="{{option[textProperty]}}" >',
+						'<i data-ng-if="image == \'true\'" data-ng-style="{\'background-image\': \'url(\'+(option[imageProperty])+\')\'}">',
 						'</i>',
-						'<span data-ng-bind="option[ctrl.textProperty]">',
+						'<span data-ng-bind="option[textProperty]">',
 						'</span>',
 					'</li>',
 				'</ul>',
@@ -50,8 +50,6 @@
 			},
 			restrict: 'AEC',
 			controller: controller,
-			controllerAs: 'ctrl',
-			bindToController: true,
 			replace: true,
 			link: postLink,
 			template: template,
@@ -62,44 +60,43 @@
 
 		function controller($scope){
 
-			var vm = this;
-			vm.opened = false;
+			$scope.opened = false;
 
-			if((typeof vm.value) === 'number'){
+			if((typeof $scope.value) === 'number'){
 
-				vm.data[vm.value].selected = true;
+				$scope.data[$scope.value].selected = true;
 
 			}else{
 
-				vm.value = vm.value || 0;
+				$scope.value = $scope.value || 0;
 
 			}
 
-			vm.textProperty = vm.textProperty || 'text';
-			vm.imageProperty = vm.imagetProperty || 'image';
+			$scope.textProperty = $scope.textProperty || 'text';
+			$scope.imageProperty = $scope.imagetProperty || 'image';
 
-			vm.selectOption = selectOption;
-			vm.close = close;
-			vm.toggle = toggle;
+			$scope.selectOption = selectOption;
+			$scope.close = close;
+			$scope.toggle = toggle;
 
 			function selectOption(_index){
 
-				if(_index !== parseInt(vm.value)){
+				if(_index !== parseInt($scope.value)){
 					
-					var _pastSelected = vm.value;
+					var _pastSelected = $scope.value;
 
 					_index = parseInt(_index);
 
-					vm.value = _index;
-					vm.data[_index].selected = true;
+					$scope.value = _index;
+					$scope.data[_index].selected = true;
 
-					if(vm.data[_pastSelected]){
+					if($scope.data[_pastSelected]){
 
-						delete vm.data[_pastSelected].selected;
+						delete $scope.data[_pastSelected].selected;
 
 					}
 
-					vm.onchange();
+					$scope.onchange();
 
 					$scope.$broadcast('option-selected', {index: _index, pastIndex: _pastSelected});
 
@@ -109,14 +106,14 @@
 
 			function toggle(){
 
-				if(vm.opened){
+				if($scope.opened){
 
-					vm.opened = false;
+					$scope.opened = false;
 					$scope.$broadcast('close-dropdown');
 
 				}else{
 
-					vm.opened = true;
+					$scope.opened = true;
 					$scope.$broadcast('open-dropdown');
 
 				}
@@ -125,42 +122,42 @@
 
 			function close(){
 
-				vm.opened = false;
+				$scope.opened = false;
 				$scope.$broadcast('close-dropdown');
 
 			}
 			
 		}
 
-		function postLink($scope, $element, attrs, ctrl){
+		function postLink($scope, $element, attrs){
 
 			var options;
 			var openedClass = 'opened';
 			var selectedClass = 'selected';
 			var eventId = Math.round(Math.random() * 1000);
 
-			if(ctrl.openedClass){
-				var openedClass = ctrl.openedClass;
+			if($scope.openedClass){
+				var openedClass = $scope.openedClass;
 			}
 
-			if(ctrl.selectedClass){
-				var selectedClass = ctrl.selectedClass;
+			if($scope.selectedClass){
+				var selectedClass = $scope.selectedClass;
 			}
 			
 			$scope.$on('open-dropdown', open);
 			$scope.$on('close-dropdown', close);
 			$scope.$on('option-selected', select);
-			$scope.$on('option-selected', ctrl.close);
+			$scope.$on('option-selected', $scope.close);
 			$scope.$on('$destroy', destroy);
 
 			$element.on('click', elementClick);
-			$document.on('click', ctrl.close);
+			$document.on('click', $scope.close);
 
 			//init
 			$timeout(function(){
 
 				options = $element.find('li');
-				options.eq(ctrl.value).addClass(selectedClass);
+				options.eq($scope.value).addClass(selectedClass);
 
 			});
 
