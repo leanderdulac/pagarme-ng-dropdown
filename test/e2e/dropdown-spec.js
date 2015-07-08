344a8ad8eeca4db00222bfff0f00de0b37deda84
Rafael Violato <rfviolato@gmail.com>
2015-07-08 10:04:04 -0300

    Add select option test

--- test/e2e/dropdown-spec.js
+++ test/e2e/dropdown-spec.js
@@ -22,10 +22,12 @@ describe('Dropdown Spec', function(){
 	it('should open dropdown on click', clickOpen);
 	it('should open dropdown on enter key press', enterKeyOpen);
 	it('should close dropdown on esc key press', escKeyClose);
+	it('should select an option when clicked', clickSelectOption);
 
 	function hasDropdowns(){
 
-		elements.dropdowns.then(function(dropdowns){
+		elements.dropdowns
+		.then(function(dropdowns){
 
 			expect(dropdowns.length).toBeGreaterThan(0);
 			
@@ -35,9 +37,11 @@ describe('Dropdown Spec', function(){
 
 	function hasOptions(){
 
-		elements.dropdowns.then(function(dropdowns){
+		elements.dropdowns
+		.then(function(dropdowns){
 
-			dropdowns[0].all(by.repeater(elements.optionsRepeater)).then(function(options){
+			dropdowns[0].all(by.repeater(elements.optionsRepeater))
+			.then(function(options){
 
 				expect(options.length).toBeGreaterThan(0);
 				
@@ -72,7 +76,8 @@ describe('Dropdown Spec', function(){
 
 		_openEnterDropdown(function(dropdown){
 
-			browser.actions().sendKeys(protractor.Key.ESCAPE).perform().then(function(){
+			browser.actions().sendKeys(protractor.Key.ESCAPE).perform()
+			.then(function(){
 
 				expect(dropdown.getAttribute('class')).not.toMatch(elements.openedClass);
 				
@@ -84,14 +89,17 @@ describe('Dropdown Spec', function(){
 
 	function tabFocus(){
 
-		elements.dropdowns.then(function(dropdowns){
+		elements.dropdowns
+		.then(function(dropdowns){
 
 			var _dropdown = dropdowns[0];
 
-			browser.actions().sendKeys(protractor.Key.TAB).perform().then(function(){
+			browser.actions().sendKeys(protractor.Key.TAB).perform()
+			.then(function(){
 
 				browser.driver.switchTo().activeElement()
-				.getAttribute('class').then(function(focusedClasses){
+				.getAttribute('class')
+				.then(function(focusedClasses){
 
 					expect(_dropdown.getAttribute('class')).toMatch(focusedClasses);
 					
@@ -103,13 +111,54 @@ describe('Dropdown Spec', function(){
 		
 	}
 
+	function clickSelectOption(){
+
+		_openClickDropdown(0, function(dropdown){
+
+			dropdown.all(by.repeater(elements.optionsRepeater))
+			.then(function(options){
+
+				var _option = options[0];
+
+				_checkVisible(_option)
+				.then(function(){
+
+					_option.element(by.css('span')).getText()
+					.then(function(previousText){
+
+						_option.click()
+						.then(function(){
+
+							var _current = dropdown.element(by.css('.current-selected-option span'));
+
+							_current.getText()
+							.then(function(currentText){
+
+								expect(currentText).toMatch(previousText);
+								
+							});
+							
+						});
+						
+					});
+					
+				});				
+				
+			});
+			
+		});
+		
+	}
+
 	function _openEnterDropdown(callback){
 
-		browser.actions().sendKeys(protractor.Key.TAB).perform().then(function(){
+		browser.actions().sendKeys(protractor.Key.TAB).perform()
+		.then(function(){
 
 			var _focused = browser.driver.switchTo().activeElement();
 
-			browser.actions().sendKeys(protractor.Key.ENTER).perform().then(function(){
+			browser.actions().sendKeys(protractor.Key.ENTER).perform()
+			.then(function(){
 
 				callback(_focused);
 				
@@ -121,11 +170,13 @@ describe('Dropdown Spec', function(){
 
 	function _openClickDropdown(index, callback){
 
-		elements.dropdowns.then(function(dropdowns){
+		elements.dropdowns
+		.then(function(dropdowns){
 
 			var _dropdown = dropdowns[index];
 
-			_dropdown.click().then(function(){
+			_dropdown.click()
+			.then(function(){
 
 				callback(_dropdown);
 				
@@ -133,6 +184,16 @@ describe('Dropdown Spec', function(){
 			
 		});
 		
+	}
+
+	function _checkVisible(elem){
+
+		return browser.wait(function () {
+		    
+		    return elem.isDisplayed();
+
+		},15000);
+
 	}
 
 
