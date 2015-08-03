e34b75ace4d40a56d5fe9f63d460af240f7ccd8c
Rafael Violato <rfviolato@gmail.com>
2015-08-03 10:58:27 -0300

    Update Readme.md

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,9 @@ angular.module('pg-ng-dropdown-demo', ['pg-ng-dropdown'])
 	var a = 0;
 	var b = 1;
 
-	console.log(a !== b);
-
 	$scope.testDisabled = function(){
 
 		return a !== b;
-		
-	};
-
-	window.t = function(){
-
-		b = 0;
 		
 	};
 	
